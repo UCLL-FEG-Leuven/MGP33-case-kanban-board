7c30986c6d8c9694d4f1cbd436b8b93ef74cd8ec
DavidUrting <david.urting@gmail.com>
2024-02-22 23:55:32 +0100

    betere versies (zonder id's)

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -70,52 +70,38 @@ export class Ticket {
         // Koppelen van drag event handlers.
         this.#wireDragAndDropEventHandlers(liHtmlElement);
 
-        const titleHeadingId = `ticket-title-heading-${this.id}`;
-        const titleFormId = `ticket-title-form-${this.id}`;
-        const titleInputId = `ticket-title-input-${this.id}`;
-
-        const descriptionParagraphId = `ticket-description-paragraph-${this.id}`;
-        const descriptionFormId = `ticket-description-form-${this.id}`;
-        const descriptionTextareaId = `ticket-description-textarea-${this.id}`;
-
-        const personSpanId = `ticket-person-span-${this.id}`;
-        const personFormId = `ticket-person-form-${this.id}`;
-        const personSelectId = `ticket-person-select-${this.id}`;
-
         // Omdat we gebruik maken van innerHTML (en niet van createElement)
         // is het koppelen van event listeners iets omslachtiger.
         // Maar de code is wel leesbaarder...
         liHtmlElement.innerHTML = `
-        <h3 id="${titleHeadingId}" class="ticket-title">
-            <span>${this.title}</span>
-        </h3>
-        <form id="${titleFormId}" class="ticket-title" style="display: none">
-            <input id="${titleInputId}" type="text" value="${this.title}" />
-            <input type="submit" value="Ok" />
-        </form>
-        <p id="${descriptionParagraphId}" class="ticket-description">${this.description}</p>
-        <form id="${descriptionFormId}" class="ticket-description" style="display: none">
-            <textarea id="${descriptionTextareaId}" rows="2" cols="50">${this.description}</textarea>
-            <input type="submit" value="Ok" />
-        </form>        
-        <span id="${personSpanId}" class="ticket-person">
-            ${this.person ? this.person.firstName : 'unassigned'}
-        </span>
-        <form id="${personFormId}" class="ticket-person" style="display: none">
-            <select id="${personSelectId}">
-                <option value="" selected>Selecteer een persoon...</option>
-                ${(await getAllPersons()).reduce((accumulator, currentPerson) => accumulator + `<option value="${currentPerson.id}">${currentPerson.firstName}</option>`,"")}
-            </select>
-        </form>`;
+            <h3 class="ticket-title">${this.title}</h3>
+            <form class="ticket-title" style="display: none">
+                <input type="text" value="${this.title}" />
+                <input type="submit" value="Ok" />
+            </form>
+            <p class="ticket-description">${this.description}</p>
+            <form class="ticket-description" style="display: none">
+                <textarea rows="2" cols="50">${this.description}</textarea>
+                <input type="submit" value="Ok" />
+            </form>        
+            <span class="ticket-person">
+                ${this.person ? this.person.firstName : 'unassigned'}
+            </span>
+            <form class="ticket-person" style="display: none">
+                <select>
+                    <option value="" selected>Selecteer een persoon...</option>
+                    ${(await getAllPersons()).reduce((accumulator, currentPerson) => accumulator + `<option value="${currentPerson.id}">${currentPerson.firstName}</option>`,"")}
+                </select>
+            </form>`;
 
         // Bij het appenden zal de browser de innerHTML parsen en zijn de nieuwe DOM elementen 
         // beschikbaar. Het is pas ook op dit moment dat event listeners kunnen gekoppeld worden
         // (als er dus gebruik wordt gemaakt van innerHTML).
         columnTicketsContainerHtmlElement.appendChild(liHtmlElement);
 
-        this.#wireTitleEventHandlers(titleHeadingId, titleFormId, titleInputId);
-        this.#wireDescriptionEventHandlers(descriptionParagraphId, descriptionFormId, descriptionTextareaId);       
-        this.#wirePersonEventHandlers(personSpanId, personFormId, personSelectId);
+        this.#wireTitleEventHandlers();
+        this.#wireDescriptionEventHandlers();       
+        this.#wirePersonEventHandlers();
     }
 
     #wireDragAndDropEventHandlers(liHtmlElement) {
@@ -124,84 +110,96 @@ export class Ticket {
         });
     }
 
-    #wireTitleEventHandlers(titleHeadingId, titleFormId, titleInputId) {
+    #wireTitleEventHandlers() {
+        const titleHeading = document.querySelector(`#ticket-${this.id} h3.ticket-title`);
+        const titleForm = document.querySelector(`#ticket-${this.id} form.ticket-title`);
+        const titleInput = document.querySelector(`#ticket-${this.id} form.ticket-title input[type='text']`);
+
         // Bij het klikken op de titel moet een form met een input field getoond worden.
         // Voor het gebruiksgemak krijgt die input ook al direct de focus.
-        document.getElementById(titleHeadingId).addEventListener("click", (e) => {
-            document.getElementById(titleHeadingId).style.display = "none";
-            document.getElementById(titleFormId).style.display = "block";
-            document.getElementById(titleInputId).focus();
+        titleHeading.addEventListener("click", (e) => {
+            titleHeading.style.display = "none";
+            titleForm.style.display = "block";
+            titleInput.value = this.title;
+            titleInput.focus();
         });
 
         // Form om de titel aan te passen
-        document.getElementById(titleFormId).addEventListener("submit", (e) => {
+        titleForm.addEventListener("submit", (e) => {
             // Dit zorgt ervoor dat er geen postback van de form gebeurt (anders ben je alles kwijt!)
             e.preventDefault();
 
             // nieuwe titel uitlezen en onthouden + tonen in de header.
-            this.#title = document.getElementById(titleInputId).value;
-            document.querySelector(`#${titleHeadingId} span`).innerText = this.#title;
+            this.#title = titleInput.value;
+            titleHeading.innerText = this.#title;
 
             // En de form weer verbergen en de header tonen.
-            document.getElementById(titleHeadingId).style.display = "block";
-            document.getElementById(titleFormId).style.display = "none";
+            titleHeading.style.display = "block";
+            titleForm.style.display = "none";
         });        
     }
 
-    #wireDescriptionEventHandlers(descriptionParagraphId, descriptionFormId, descriptionTextareaId) {
+    #wireDescriptionEventHandlers() {
+        const descriptionParagraph = document.querySelector(`#ticket-${this.id} p.ticket-description`);
+        const descriptionForm = document.querySelector(`#ticket-${this.id} form.ticket-description`);
+        const descriptionTextarea = document.querySelector(`#ticket-${this.id} form.ticket-description textarea`);
+
         // Bij het klikken op de description moet een form met een textarea field getoond worden.
         // Voor het gebruiksgemak krijgt die textarea ook al direct de focus.
-        document.getElementById(descriptionParagraphId).addEventListener("click", (e) => {
-            document.getElementById(descriptionParagraphId).style.display = "none";
-            document.getElementById(descriptionFormId).style.display = "block";
-            document.getElementById(descriptionTextareaId).focus();
+        descriptionParagraph.addEventListener("click", (e) => {
+            descriptionParagraph.style.display = "none";
+            descriptionForm.style.display = "block";
+            descriptionTextarea.focus();
         });
 
         // Form om de description aan te passen
-        document.getElementById(descriptionFormId).addEventListener("submit", (e) => {
+        descriptionForm.addEventListener("submit", (e) => {
             // Dit zorgt ervoor dat er geen postback van de form gebeurt (anders ben je alles kwijt!)
             e.preventDefault();
 
             // nieuwe description uitlezen en onthouden + tonen in de header.
-            this.#description = document.getElementById(descriptionTextareaId).value;
-            document.querySelector(`#${descriptionParagraphId}`).innerText = this.#description;
+            this.#description = descriptionTextarea.value;
+            descriptionParagraph.innerText = this.#description;
 
             // En de form weer verbergen en de header tonen.
-            document.getElementById(descriptionParagraphId).style.display = "block";
-            document.getElementById(descriptionFormId).style.display = "none";
+            descriptionParagraph.style.display = "block";
+            descriptionForm.style.display = "none";
         });        
     }
 
-    #wirePersonEventHandlers(personSpanId, personFormId, personSelectId) {
+    #wirePersonEventHandlers() {
+        const personSpan = document.querySelector(`#ticket-${this.id} span.ticket-person`);
+        const personForm = document.querySelector(`#ticket-${this.id} form.ticket-person`);
+        const personSelect = document.querySelector(`#ticket-${this.id} form.ticket-person select`);
+
         // Bij het klikken op de person moet een form met een select field getoond worden.
         // Voor het gebruiksgemak krijgt die select ook al direct de focus.
-        document.getElementById(personSpanId).addEventListener("click", (e) => {
-            document.getElementById(personSpanId).style.display = "none";
-            document.getElementById(personFormId).style.display = "block";
-            document.getElementById(personSelectId).focus();
+        personSpan.addEventListener("click", (e) => {
+            personSpan.style.display = "none";
+            personForm.style.display = "block";
+            personSelect.focus();
         });
 
         // Van zodra de gebruiker een nieuwe persoon heeft geselecteerd submitten we de form 
         // direct...
-        document.getElementById(personSelectId).addEventListener("change", (e) => {
-            document.getElementById(personFormId).requestSubmit();
+        personSelect.addEventListener("change", (e) => {
+            personForm.requestSubmit();
         });
 
         // Form om de person aan te passen
-        document.getElementById(personFormId).addEventListener("submit", async (e) => {
+        personForm.addEventListener("submit", async (e) => {
             // Dit zorgt ervoor dat er geen postback van de form gebeurt (anders ben je alles kwijt!)
             e.preventDefault();
 
             // nieuwe person uitlezen en onthouden + tonen in de header.
             let persons = await getAllPersons();
-            let selectedPersonId = parseInt(document.getElementById(personSelectId).value);
-            console.log(selectedPersonId);
+            let selectedPersonId = parseInt(personSelect.value);
             this.#person = persons.filter(p => p.id === selectedPersonId)[0];
-            document.querySelector(`#${personSpanId}`).innerText = this.#person.firstName;
+            personSpan.innerText = this.#person.firstName;
 
             // En de form weer verbergen en de header tonen.
-            document.getElementById(personSpanId).style.display = "block";
-            document.getElementById(personFormId).style.display = "none";
+            personSpan.style.display = "block";
+            personForm.style.display = "none";
         });
     }    
 }
